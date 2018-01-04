a26a07654af58fd77a91dfa0173a2f51893580ab
djrhee08 <djrhee08@gmail.com>
2018-01-04 11:52:49 -0600

    inpoly_test.m first draft

--- inpoly_test.m
+++ inpoly_test.m
@@ -1,7 +1,7 @@
 clear all
 clc
 close all
-%%
+%% Extract useful data from DICOM image files
 list = dir('MRI1/*.dcm');
 for i = 1:length(list)
     if strcmp(list(i).name,'str.dcm') == 1
@@ -18,7 +18,7 @@ img_pixsize = [img_pixsize;1]';
 img_pos_temp = img_pos(1,:);
 img_pos_temp(3) = 0;
 img_size = [info.Rows, info.Columns];
-%%
+%% Extract useful data from RT Structure file
 item = struct2cell(str.ROIContourSequence);
 item_name = struct2cell(str.StructureSetROISequence);
 index = 1;
@@ -41,28 +41,71 @@ for i=1:length(item)
     end
 end
 %%
-%for i=1:length(data_total)
-img_temp = img(:,:,13);
-contour_temp = data_temp(1:36,:);
-
-figure;
-imagesc(img_temp)
-colormap gray
-hold on
-plot(contour_temp(:,1),contour_temp(:,2))
-hold off
-%%
 qq = double(0:img_size(1)-1);
 qq = qq + 0.5;
-xq = repmat(qq,512,1);
-yq = repmat(qq',1,512);
+xq = repmat(qq,img_size(1),1);
+yq = repmat(qq',1,img_size(2));
 
-xv = contour_temp(:,1);
-yv = contour_temp(:,2);
+for i=1:length(data_total)
+    mask{i}.data = false(size(img));
+    mask{i}.name = data_name{i};
+    contour_temp = data_total{i};
+    cpoint = logical(diff(contour_temp(:,3))); % When z coord changes
+    index_start = 1; index_end = 1;
+    for j=1:length(contour_temp)-1
+        if cpoint(j) == 0
+            index_end = index_end + 1;
+        elseif cpoint(j) == 1
+            z = find(img_pos(:,3) == contour_temp(j,3));
+            % Create mask
+            xv = contour_temp(index_start:index_end,1);
+            yv = contour_temp(index_start:index_end,2);
 
-[in,on] = inpolygon(xq,yq,xv,yv);
+            in = inpolygon(xq,yq,xv,yv);
+            mask{i}.data(:,:,z) = in;
+            
+            % Prepare for the next indices
+            index_end = index_end + 1;
+            index_start = index_end;
+        end
+    end      
+end
 
-img_temp(in) = 5000;
+%%
+c1 = mask{1}.data;
+c2 = mask{2}.data;
+c3 = mask{3}.data;
+c4 = mask{4}.data;
+
 figure;
-imagesc(img_temp)
-colormap gray
+img(c2) = 10000;
+for i=1:30
+    imagesc(img(:,:,i))
+    colormap gray
+    pause(0.3)
+end
+
+% img_temp = img(:,:,13);
+% contour_temp = data_temp(1:36,:);
+% 
+% figure;
+% imagesc(img_temp)
+% colormap gray
+% hold on
+% plot(contour_temp(:,1),contour_temp(:,2))
+% hold off
+%%
+% qq = double(0:img_size(1)-1);
+% qq = qq + 0.5;
+% xq = repmat(qq,512,1);
+% yq = repmat(qq',1,512);
+% 
+% xv = contour_temp(:,1);
+% yv = contour_temp(:,2);
+% 
+% [in,on] = inpolygon(xq,yq,xv,yv);
+% 
+% img_temp(in) = 5000;
+% figure;
+% imagesc(img_temp)
+% colormap gray
